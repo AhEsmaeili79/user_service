6276686afafa677a225ff2edc8cda267b2593d18
amirhossein <ah.esmaeili.79@gmail.com>
2025-07-05 18:05:41 +0330

    update : update user, otp model

--- app/models/otp_code.py
+++ app/models/otp_code.py
@@ -1,8 +1,9 @@
 import uuid
-from sqlalchemy import Column, String, DateTime, Boolean, ForeignKey
-from sqlalchemy.dialects.sqlite import UUID
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
+from sqlalchemy.dialects.sqlite import UUID
+from sqlalchemy import Column, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
     
@@ -11,7 +12,7 @@ class OtpCode(Base):
     
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     user_id = Column(UUID(as_uuid=True),ForeignKey("users.id", ondelete="CASCADE"),nullable=False)
-    code = Column(String,nullable=False)
+    ode = Column(String(5), nullable=False)  
     expires_at = Column(DateTime(timezone=True),nullable=False,index=True)
     is_used = Column(Boolean,nullable=False,default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,9 @@
-import uuid
-from sqlalchemy import Column, String, DateTime, Enum
-from sqlalchemy.dialects.sqlite import UUID
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql import func
 import enum
+import uuid
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.sqlite import UUID
+from sqlalchemy import Column, String, DateTime, Enum
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -15,14 +15,14 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    name = Column(String, nullable=False, index=True)
-    phone_number = Column(String, unique=True, nullable=False, index=True)
+    name = Column(String(100), nullable=False, index=True)  # Specify a maximum length for name
+    phone_number = Column(String(15), unique=True, nullable=False, index=True)  # Limit phone number length
     password_hash = Column(String, nullable=False)
     avatar_url = Column(String, nullable=True)
     card_number = Column(String, nullable=True)
     card_holder_name = Column(String, nullable=True)
-    role = Column(Enum(UserRole), nullable=False)
-    email = Column(String, unique=True, nullable=False, index=True)
+    role = Column(Enum(UserRole), nullable=False, default=UserRole.user)  # Set default role
+    email = Column(String(255), unique=True, nullable=False, index=True)  # Limit email length
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False, index=True)
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False) 
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
